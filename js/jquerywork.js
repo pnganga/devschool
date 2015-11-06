5f5fa72cdfc72448a937c22c47bb1bf144d68d18
pnganga <pnganga05@gmail.com>
2015-11-06 09:05:20 -0500

    changed the structure of my directory and also added to my source code

--- js/jquerywork.js
+++ js/jquerywork.js
@@ -8,30 +8,30 @@ $(document).ready(function()
 
 		 });
 
-		$("#project1 h5").hide();
+		// $("#project1 h5").hide();
 
-		$("#project1  img").mouseenter(function()
-			{
+		// // $("#project1  img").mouseenter(function()
+		// 	{
 
-				$("#project1 h5").show(150);
+		// 		// $("#project1 h5").show(150);
 
-			});
-		$("#project1 img").mouseleave(function()
-			{
-				$("#project1 h5").hide(150);
+		// 	});
+		// // $("#project1 img").mouseleave(function()
+		// 	{
+		// 		$("#project1 h5").hide(150);
 
-			});
-		$("#project1 img").mouseenter(function()
+		// 	});
+		// // $("#project1 img").mouseenter(function()
 
-			{
-				$(".container h2").css("color", "#C6FF00");
+		// 	{
+		// 		$(".container h2").css("color", "#C6FF00");
 
-			});
-		$("#project1 img").mouseleave(function()
-			{
-				$(".container h2").css("color", "#1565C0")
+		// // 	});
+		// // $("#project1 img").mouseleave(function()
+		// 	{
+		// 		$(".container h2").css("color", "#1565C0")
 
-			});
+		// 	});
 
 		$("#contact-form").on("submit",function(e)
 		{
@@ -71,9 +71,20 @@ $(document).ready(function()
 
 					}
 
+				},
+				highlight: function(element)
+				{
+					$(element).addClass("error");
+
+
+				
 				}
 
 
 			});
+
+             $( "#accordion" ).accordion();
+ 
+		
 		
 	});
