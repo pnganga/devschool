75c924a86ed4107456ce9d6e21393cc738c28830
pnganga <pnganga05@gmail.com>
2015-12-01 09:09:14 -0500

    another commmit

--- js/appcontroller.js
+++ js/appcontroller.js
@@ -16,20 +16,6 @@ myApp.controller('myCars', function($scope){
 		{brand:"Nissan",model:"Wingroad",type:"StationWagon",year:"2010",image:"../images/wingroad.jpg"},
 		{brand:"Subaru",model:"Legacy",type:"Sports",year:"2008",image:"../images/legacy.jpg"},
 		{brand:"Range Rover",model:"Sport",type:"SUV",year:"2015",image:"../images/sport.jpg"}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	];
 
 
@@ -42,5 +28,32 @@ myApp.controller('userController', function($scope, $http){
 	$scope.formSubmit = function(){
 
 		console.log($scope.user);
+		// call your node API
+		$http.post ('', $scope.user).success(function(res){
+			console.log("server found");
+
+
+
+		})
+
+
+		.error(function(res){
+				console.log("server not found");
+
+
+
+		});
+		// communicate with your NODE API
+		// pass your user object to the API
 	};
+});
+
+
+myApp.controller('customersCtrl', function($scope, $http) {
+  $http.get("http://www.w3schools.com/angular/customers.php")
+  .success(function (response) {
+  	console.log('hi');
+  	$scope.movies = response;
+
+  });
 });
